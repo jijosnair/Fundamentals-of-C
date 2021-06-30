5143923a8e48b59d305e14a938dde5af97ed860d
jijosnair <jijosnair@gmail.com>
2021-06-29 23:03:42 -0700

    initialcommit

---
+++ function1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void sum();
+void sum()
+{
+int n1,n2,s;
+printf("Enter first No:");
+scanf("%d",&n1);
+printf("Enter Second No:");
+scanf("%d",&n2);
+s=n1+n2;
+printf("Sum of %d and %d is %d", n1,n2,s);
+
+}
+int main(int argc, char const *argv[])
+{
+    sum();
+    return 0;
+}
